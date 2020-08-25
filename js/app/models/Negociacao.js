a6b86ad5432f58408b8729013ba3b05f7f40ea7f
IgornJ17 <39203490+IgornJ17@users.noreply.github.com>
2020-08-25 17:22:49 -0300

    Aula 2 JS

---
+++ js/app/models/Negociacao.js
@@ -0,0 +1,25 @@
+class Negociacao {
+    constructor(date, qtd, valor){
+        this._date = new Date(date.getTime());
+        this._qtd = qtd;
+        this._valor = valor;
+        this._volume = this.qtd * this.valor;
+        Object.freeze(this);
+    }
+     
+    get volume(){
+        return this._volume;
+    }
+
+    get data(){
+        return this._date;
+    }
+
+    get qtd(){
+        return this._qtd;
+    }
+
+    get valor(){
+        return this._valor;
+    }
+}
